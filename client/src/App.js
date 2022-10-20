8720a777992ce1f386d41f32e6abb9d3012a5c65
Khris Bharmmano <84142253+khris-cpu@users.noreply.github.com>
2022-10-20 14:03:50 +0700

    Ready for Awesome

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = { passwords: [] }
+
+  componentDidMount() {
+    this.getPasswords();
+  }
+
+  getPasswords = () => {
+    fetch('/api/passwords')
+      .then(res => res.json())
+      .then(passwords => this.setState({ passwords }));
+  }
+
+  render() {
+    const { passwords } = this.state;
+
+    return (
+      <div className="text-center font-tight w-full">
+        {passwords.length ? (
+          <div>
+            <h1 className="font-tight text-[42px]">5 Passwords.</h1>
+            <ul className="list-none p-0 text-[32px] mb-[2em]">
+              {passwords.map((password, index) =>
+                <li key={index}>
+                  {password}
+                </li>
+              )}
+            </ul>
+            <button className="text-[32px] font-tight border-[2px] border-[solid] border-[#000] bg-[#fff] p-[10px_25px] hover:bg-[#FDD836] active:bg-[#FFEFA9]" onClick={this.getPasswords}>
+              Get More
+            </button>
+          </div>
+        ) : (
+          <div>
+            <h1 className="font-tight text-[42px]">No passwords :(</h1>
+            <button
+              className="text-[32px] font-tight border-[2px] border-[solid] border-[#000] bg-[#fff] p-[10px_25px] hover:bg-[#FDD836]"
+              onClick={this.getPasswords}>
+              Try Again?
+            </button>
+          </div>
+        )}
+      </div>
+    )
+  }
 }
 
 export default App;
